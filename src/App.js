731f759be9684bc9b2765587ee6e866173d896ca
Rajesh Rawat <rawatrajesh0519@gmail.com>
2021-10-09 20:19:33 +0530

    v- 1.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Services from './components/Services'
+import Gallery from './components/Gallery'
+import SeeDo from './components/SeeDo'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <>
+          <Navbar />
+
+          <Home />
+
+          <About />
+
+          <Services />
+
+          <Gallery />
+
+          <SeeDo />
+
+          <Contact />
+
+          <Footer />
+
+       </>
   );
 }
 
