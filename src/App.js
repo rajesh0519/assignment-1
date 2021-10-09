fbb47f93da4d2003cfab52ed1a67c0981da8105b
Rajesh Rawat <rawatrajesh0519@gmail.com>
2021-10-09 22:01:02 +0530

    v-1.0

--- src/App.js
+++ src/App.js
@@ -11,10 +11,43 @@ import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCBx2Q_-JCDUlLdludMO9art2yVd475Mwo",
+
+  authDomain: "assignment-1-11002.firebaseapp.com",
+
+  projectId: "assignment-1-11002",
+
+  storageBucket: "assignment-1-11002.appspot.com",
+
+  messagingSenderId: "286388846643",
+
+  appId: "1:286388846643:web:04a56c145f01aca3f1a346"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
 
 
 
 function App() {
+
   return (
        <>
           <Navbar />
@@ -32,6 +65,8 @@ function App() {
           <Contact />
 
           <Footer />
+
+
 
        </>
   );
